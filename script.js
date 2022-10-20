fbcf65a9594b3431954058af28e5055e9fb39428
Ariel Bustos <ariel.bustos02@inacapmail.cl>
2022-10-19 22:38:18 -0300

    portafolio

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ window.onscroll = function() { efectoHabilidades() };
 function efectoHabilidades() {
     var skills = document.getElementById("skills");
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
-    if (distancia_skills >= 200) {
+    if (distancia_skills >= 2-00) {
         document.getElementById("html").classList.add("barra-progreso1");
         document.getElementById("js").classList.add("barra-progreso2");
         document.getElementById("bd").classList.add("barra-progreso3");
