a98208e08dc94d50dd8ef4ff1cb256fb62eb4513
Ariel Bustos <ariel.bustos02@inacapmail.cl>
2022-10-21 11:37:52 -0300

    Ejemplo

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Función que me aplica el estilo a la opciòn seleccionada y quita la previamente seleccionada
 function seleccionar(link) {
-    var opciones = document.querySelectorAll('#links  a');
+    let opciones = document.querySelectorAll('#links  a');
     opciones[0].className = "";
     opciones[2].className = "";
     opciones[3].className = "";
@@ -9,13 +9,13 @@ function seleccionar(link) {
 
     //Hacemos desaparecer el menu una vez que se ha seleccionado una opcion
     //en modo responsive
-    var x = document.getElementById("nav");
+    let x = document.getElementById("nav");
     x.className = "";
 }
 
 //función que muestra el menu responsive
 function responsiveMenu() {
-    var x = document.getElementById("nav");
+    let x = document.getElementById("nav");
     if (x.className === "") {
         x.className = "responsive";
     } else {
@@ -28,8 +28,8 @@ window.onscroll = function() { efectoHabilidades() };
 
 //funcion que aplica la animación de la barra de habilidades
 function efectoHabilidades() {
-    var skills = document.getElementById("skills");
-    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    let skills = document.getElementById("skills");
+    let distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if (distancia_skills >= 2-00) {
         document.getElementById("html").classList.add("barra-progreso1");
         document.getElementById("js").classList.add("barra-progreso2");
